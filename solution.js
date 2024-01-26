e32b719839d547cce3f0be0fac1edce572b5b280
waderice52224 <waderice52224@gmail.com>
2024-01-26 12:55:59 -0700

    Added new high functions and new predicate functions

--- solution.js
+++ solution.js
@@ -1,22 +1,3 @@
-
-let count1 = 0;
-let count2 = 0;
-for(let i = 0; i < transactions.length; i++){
-    if(transactions[i].amount == 0 || transactions[i].amount == null){
-        // console.log(transactions[i].id + " is invalid, " + transactions[i].amount)
-        // console.log(transactions[i])
-        count1++;
-        continue;
-        //FIG_JAM, FIG_JELLY, SPICY_FIG_JAM, or ORANGE_FIG_JELLY.
-    }else if(transactions[i].product != 'FIG_JAM' && transactions[i].product != 'FIG_JELLY' && transactions[i].product != 'SPICY_FIG_JAM' && transactions[i].product != 'ORANGE_FIG_JELLY'){
-        // console.log(transactions[i].product)
-        count2++;
-    }
-}
-
-
-console.log("There are "+ count1 + " transactions that have a null amount, and "+ count2 + " have an invalid product name for a total of " + (count1 + count2) + " bad transactions")
-
 function filter(data, predicate) {
     const result = [];
   
@@ -28,18 +9,45 @@ function filter(data, predicate) {
   
     return result;
   }
+
+  function findlast(data, predicate) {
+    result = [];
+    for (const item of data) {
+      if (predicate(item)) {
+        result = item;
+      }
+    }
   
-  // Custom predicate function
+    return result;
+  }
+
+  function map(data, callBack) {
+    result = [];
+    for (const item of data) {
+      result.push(callBack(item))
+    }
+  
+    return result;
+  }
+  
+
   function isBadPrice(transaction) {
-    return transaction.amount === null;
+    return transaction.amount === null || transaction.amount === 0;
   }
 
   function isBadProduct(transaction) {
     return transaction.product != 'FIG_JAM' && transaction.product != 'FIG_JELLY' && transaction.product != 'SPICY_FIG_JAM' && transaction.product != 'ORANGE_FIG_JELLY';
   }
+  function toString(transaction) {
+    return `${transaction}`
+  }
+  
   
   
   const badPrices = filter(transactions, isBadPrice);
   const badProducts = filter(transactions, isBadProduct);
+  const badPricesLast = findlast(transactions, isBadPrice);
+  const toStringBD = map(badPrices, toString);
   
-  console.log("There are "+ badPrices.length + " transactions that have a null amount, and "+ badProducts.length + " have an invalid product name for a total of " + (count1 + count2) + " bad transactions")
+  console.log("There are "+ badPrices.length + " transactions that have a null amount, and "+ badProducts.length + " have an invalid product name for a total of " + (badPrices.length + badProducts.length) + " bad transactions")
+  console.log(badPricesLast)
