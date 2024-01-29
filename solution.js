2e464426fc3a49f7a39268388cb8c712a569cc74
waderice52224 <waderice52224@gmail.com>
2024-01-29 12:40:30 -0700

    Done

--- solution.js
+++ solution.js
@@ -37,11 +37,14 @@ function filter(data, predicate) {
   }
   function pairIf(data1, data2, predicate){
     paired = [];
-    for (const item of data1) {
-        for (const item of data2) {
-            initialValue = predicate(item, initialValue)
+    for (const item1 of data1) {
+        for (const item2 of data2) {
+            if (predicate(item1, item2)){
+                paired.push([item1, item2]);
+            }
           }
       }
+      return paired;
   }
 
 
@@ -69,7 +72,30 @@ function filter(data, predicate) {
     }
     return acc;
  }
-
+ function isSame(item1, item2){
+    if(item1.emailAddress === item2.emailAddress){
+        if(item1.id !== item2.id){
+            return true;
+        }else{
+            false;
+        }
+    }
+}
+  function isIDSame(item1, item2){
+     return item1.customerId === item2.id;
+  }
+  function isInList(item, initialValue){
+    if(!initialValue.includes(item[1])){
+        initialValue.push(item[1])
+    }
+    return initialValue;
+  }
+  function customerName(item){
+    return item.firstName + " " + item.lastName;
+  }
+  
+        
+ 
   
   const { small, medium, large } = reduce(transactions, tranactionSize, {small:[], medium:[], large:[]});
   const smallFiltered = small.length - filter(small, isBadPrice).length - filter(small, isBadProduct).length;
@@ -79,9 +105,16 @@ function filter(data, predicate) {
   const badProducts = filter(transactions, isBadProduct);
   const invalidProducts = badPrices.length + badProducts.length;
   const lastOver200 = findlast(transactions, isPriceOver200);
+  const duplicates = pairIf(customers, customers, isSame);
+  const transactionsOver200 = filter(transactions, isPriceOver200);
+  const pair200s = pairIf(transactionsOver200, customers, isIDSame)
+  const reduced200 = reduce(pair200s, isInList, [])
+  const map200s = map(reduced200, customerName)
   console.log("Number of invalid transactions: " + invalidProducts);
-  console.log("Number of duplicate customers: ");
+  console.log("Number of duplicate customers: " + duplicates.length/4);
   console.log("Most recent transaction over $200: $" + lastOver200.amount);
   console.log("Number of small transactions: " + smallFiltered);
   console.log("Number of medium transactions: " + medFiltered);
   console.log("Number of large transactions: " + largeFiltered);
+  console.log("Customers with transactions over $200: " + reduced200);
+  console.log("Names of customers with transactions over $200: " + map200s);
