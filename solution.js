f6d3a1b1fda2f5af0c6c5f5b3f2c27345c99d8df
waderice52224 <waderice52224@gmail.com>
2024-01-28 22:31:07 -0700

    Removed bad code

--- solution.js
+++ solution.js
@@ -29,76 +29,18 @@ function filter(data, predicate) {
   
     return result;
   }
-  function findlast(data, predicate) {
-    result = [];
-    for (const item of data) {
-      if (predicate(item)) {
-        result = item;
+  
+  function duplateCustomers(transactions){
+    let temp = [];
+    for(let i = 0; i < transactions.length; i++){
+      if(!temp.includes(transactions[i])){
+        temp.push(transactions[i])
       }
     }
-  
-    return result;
+    return temp.length
   }
-//   function pairIf(data1, data2, predicate) {
-//     const pairs = [];
-//     for (let i = 0; i < data1.length; i++) {
-//       const value1 = data1[i];
-//       const value2 = data2[i];
-  
-//       if (predicate(value1, value2)) {
-//         pairs.push([value1, value2]);
-//       }
-//     }
-  
-//     return pairs;
-//   }
 
-// function reduce(data, callback) {
-//     let accumulator = [];
-//     for (const item of data) {
-//       accumulator = callback(item, accumulator)
-//     }
-  
-//     return accumulator;
-//   }
-// Custom reduce function
-function customReduce(array, reducer, initialValue) {
-    let accumulator = initialValue;
-  
-    for (let i = 0; i < array.length; i++) {
-      accumulator = reducer(accumulator, array[i]);
-    }
-  
-    return accumulator;
-  }
-  
-  // Predicate to categorize transactions
-  function categorizeTransaction(transactionAmount) {
-    if (transactionAmount < 25) {
-      return 'small';
-    } else if (transactionAmount >= 25 && transactionAmount <= 75) {
-      return 'medium';
-    } else {
-      return 'large';
-    }
-  }
-  
-  
-  // Use custom reduce function to count transactions by category
-  const result = customReduce(
-    filter(transactions, isBadProduct),
-    (accumulator, transaction) => {
-      const category = categorizeTransaction(transaction.amount);
-      accumulator[category]++;
-      return accumulator;
-    },
-    { small: 0, medium: 0, large: 0 }
-  );
-  
-  console.log(result);
-  
-  
-
+  //predicates
   function isBadPrice(transaction) {
     return transaction.amount === null || transaction.amount === 0;
   }
@@ -112,18 +54,7 @@ function customReduce(array, reducer, initialValue) {
  function isPriceOver200(transaction){
     return transaction.amount > 200;
  }
- function tranactionSize(item, acc){
-    const { small, medium, large } = result;
-    if (item.amount < 25) {
-      result.small.push(item);
-    } else if (item.amount < 75) {
-      result.medium.push(item);
-    } else {
-      result.large.push(item);
-    }
-    return result;
- }
-  
+
   
   
   const badPrices = filter(transactions, isBadPrice);
@@ -131,6 +62,5 @@ function customReduce(array, reducer, initialValue) {
   const invalidProducts = badPrices.length + badProducts.length;
   const lastOver200 = findlast(transactions, isPriceOver200);
   console.log("Number of invalid transactions: " + invalidProducts);
-  console.log("Number of duplicate customers: TODO");
+  console.log("Number of duplicate customers: " + duplateCustomers(transactions));
   console.log("Most recent transaction over $200: $" + lastOver200.amount);
-  console.log(reduce(transactions, tranactionSize).small);
